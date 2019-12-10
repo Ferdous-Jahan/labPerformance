a083d69888552fde637d8b22a7dd63b2ac854276
Ferdous Jahan <ferdousjahan2@gmail.com>
2019-12-10 13:08:38 +0600

    insert validation client side

--- resources/views/teacher/create.blade.php
+++ resources/views/teacher/create.blade.php
@@ -10,13 +10,13 @@
         <form method="POST">
                 <input type="hidden" name="_token" value="{{csrf_token()}}">
                 <label>Name</label>
-                <textarea type="text" class="form-control" rows="1" name="name" style="resize : none;"></textarea>
+                <textarea type="text" class="form-control" rows="1" name="name" style="resize : none;" required></textarea>
                 <br><label>User Name</label>
-                <textarea type="text" class="form-control" rows="1" name="username" style="resize : none;"></textarea>
+                <textarea type="text" class="form-control" rows="1" name="username" style="resize : none;" required></textarea>
                 <br><label>ID</label>
-                <textarea type="text" class="form-control" rows="1" name="studentId" style="resize : none;"></textarea>
+                <textarea type="text" class="form-control" rows="1" name="studentId" style="resize : none;" required></textarea>
                 <br><label>Password</label>
-                <textarea type="text" class="form-control" rows="1" name="password" style="resize : none;"></textarea>
+                <textarea type="text" class="form-control" rows="1" name="password" style="resize : none;" required></textarea>
                 <input type="submit" name="submit" class="btn btn-outline-success" value="Create">
         </form>
 </body>
